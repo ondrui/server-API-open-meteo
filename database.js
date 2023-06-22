6293f1b16464dae491969424c4c678bf64b6e7c1
ondrui <ondrui@yandex.ru>
2023-06-22 18:05:07 +0300

    -NEW- add new request db

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import { createConnection } from "mysql2/promise";
 
-const model = "best_match";
-const reqTime = "2023-06-09 08:54:08";
+const model = "best_match, hmn";
+const reqTime = "2023-06-22 06:00:02";
 
 /**
  * Function query data mysql.
@@ -16,6 +16,7 @@ export const query = async (model, reqTime) => {
     user: process.env.USER_DB,
     password: process.env.PASSWORD_DB,
     database: process.env.NAME_DB,
+    timezone: process.env.TIMEZONE_DB,
   });
 
   connection.ping(function (err) {
@@ -24,8 +25,27 @@ export const query = async (model, reqTime) => {
   });
   console.log("start");
   // db table name
-  const tabName = "data_copy";
-  const sql = `SELECT runtime, ROUND(temperature_2m, 2) AS temp FROM ${tabName} WHERE request_time = '${reqTime}' AND model = '${model}' ORDER BY runtime`;
+  const tabName = "data";
+  const sql = `SELECT
+      request_time,
+      runtime,
+      ROUND(temperature_2m, 2) AS temp,
+      model
+    FROM
+      data
+    WHERE
+      (request_time = '2023-06-19 12:00:02' AND model='hmn')
+      OR
+      (request_time = '2023-06-19 12:00:02' AND model='ecmwf_ifs04')
+      OR
+      (request_time = '2023-06-19 18:00:02' AND model='icon_global')
+      OR
+      (request_time = '2023-06-20 00:00:02' AND model='icon_global')
+      OR
+      (request_time = '2023-06-20 06:00:02' AND model='hmn')
+    ORDER BY
+      runtime`;
+  // const sqlOld = `SELECT runtime, ROUND(temperature_2m, 2) AS temp, model FROM ${tabName} WHERE request_time = '${reqTime}' AND model IN ("best_match", "hmn", "icon_global", "gfs_global", "ecmwf_ifs04") ORDER BY runtime`;
   const [rows] = await connection.execute(sql).catch((error) => {
     throw error;
   });
--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const __dirname = path.dirname(__filename);
 const port = process.env.PORT ?? 3002;
 const app = express();
 
+const one = () => {
+  return "one";
+};
+
+one();
+
 app.use(express.static(path.join(__dirname, '../front/dist')));
 
 app.get('/api', (req, res) => {
@@ -16,6 +22,22 @@ app.get('/api', (req, res) => {
   res.send(result);
 });
 result.data.forEach((v) => console.log(v));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`)
