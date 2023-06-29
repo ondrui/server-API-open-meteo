7f427ceee10b49d464f20fb1ce2749ac4b47e4ed
ondrui <ondrui@yandex.ru>
2023-06-29 18:42:14 +0300

    -SKIP-

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import createError from "http-errors";
 import { createConnection } from "mysql2/promise";
 import "dotenv/config";
 
@@ -12,7 +11,6 @@ app.use(
     extended: true,
   })
 );
-app.set("view engine", "ejs");
 // db table name
 const tabName = "data";
 
@@ -47,32 +45,33 @@ app.use((req, res, next) => {
  */
 app.get("/models", async (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
-  /**
-   * Function query data mysql.
-   * @param model Weather model name.
-   * @param runTime API runtime.
-   * @returns Data object.
-   */
-  const query = async () => {
-    let str = "";
-    const dataFromFront = [
-      { model: "hmn", runTime: "2023-06-28 06:00:02" },
-      { model: "best_match", runTime: "2023-06-28 06:00:02" },
-      { model: "ecmwf_ifs04", runTime: "2023-06-28 06:00:02" },
-      { model: "gem_global", runTime: "2023-06-28 06:00:02" },
-      { model: "gfs_global", runTime: "2023-06-28 06:00:02" },
-      { model: "icon_eu", runTime: "2023-06-28 06:00:02" },
-      { model: "icon_global", runTime: "2023-06-28 06:00:02" },
-      { model: "jma_gsm", runTime: "2023-06-28 06:00:02" },
-      { model: "meteofrance_arpege_europe", runTime: "2023-06-28 06:00:02" },
-      { model: "meteofrance_arpege_world", runTime: "2023-06-28 06:00:02" },
-    ];
-    dataFromFront.forEach(
-      ({ model, runTime }) =>
-        (str += `(runtime="${runTime}" AND model="${model}") OR `)
-    );
-    str = str.slice(0, -4);
-    const sqlModelRuntime = `
+  try {
+    /**
+     * Function query data mysql.
+     * @param model Weather model name.
+     * @param runTime API runtime.
+     * @returns Data object.
+     */
+    const query = async () => {
+      let str = "";
+      const dataFromFront = [
+        { model: "hmn", runTime: "2023-06-28 06:00:02" },
+        { model: "best_match", runTime: "2023-06-28 06:00:02" },
+        { model: "ecmwf_ifs04", runTime: "2023-06-28 06:00:02" },
+        { model: "gem_global", runTime: "2023-06-28 06:00:02" },
+        { model: "gfs_global", runTime: "2023-06-28 06:00:02" },
+        { model: "icon_eu", runTime: "2023-06-28 06:00:02" },
+        { model: "icon_global", runTime: "2023-06-28 06:00:02" },
+        { model: "jma_gsm", runTime: "2023-06-28 06:00:02" },
+        { model: "meteofrance_arpege_europe", runTime: "2023-06-28 06:00:02" },
+        { model: "meteofrance_arpege_world", runTime: "2023-06-28 06:00:02" },
+      ];
+      dataFromFront.forEach(
+        ({ model, runTime }) =>
+          (str += `(runtime="${runTime}" AND model="${model}") OR `)
+      );
+      str = str.slice(0, -4);
+      const sqlModelRuntime = `
     SELECT
       runtime,
       forecast_time,
@@ -86,15 +85,16 @@ app.get("/models", async (req, res) => {
       forecast_time
       `;
 
-    const [rows] = await connection.execute(sqlModelRuntime).catch((error) => {
-      throw error;
-    });
-    if (rows.length === 0) console.log("Empty rows! Check query params!");
-    // await connection.end();
-    return rows ?? [];
-  };
+      const [rows] = await connection
+        .execute(sqlModelRuntime)
+        .catch((error) => {
+          throw error;
+        });
+      if (rows.length === 0) console.log("Empty rows! Check query params!");
+      // await connection.end();
+      return rows ?? [];
+    };
 
-  try {
     const result = await query();
     res.json(result);
   } catch (err) {
@@ -116,20 +116,24 @@ app.get("/models", async (req, res) => {
  */
 app.post("/forecast_time", async (req, res, next) => {
   res.set("Access-Control-Allow-Origin", "*");
+
+  if (!req.body || !req.body.model || !req.body.forecast_time)
+    return res.sendStatus(400);
   const { model, forecast_time } = req.body;
 
-  const query = async (str) => {
-    const [rows] = await connection.execute(str).catch((error) => {
-      throw error;
-    });
-    if (rows.length === 0) console.log("Empty rows! Check query params!");
-    // await connection.end();
-    return rows ?? [];
-  };
-  /**
-   * First query gets list runtime.
-   */
-  const sqlFirst = `SELECT
+  try {
+    const query = async (str) => {
+      const [rows] = await connection.execute(str).catch((error) => {
+        console.log("fff", error);
+      });
+      if (rows.length === 0) console.log("Empty rows! Check query params!");
+      // await connection.end();
+      return rows;
+    };
+    /**
+     * First query gets list runtime.
+     */
+    const sqlFirst = `SELECT
       runtime
     FROM
       ${tabName}
@@ -138,16 +142,16 @@ app.post("/forecast_time", async (req, res, next) => {
     ORDER BY
       forecast_time`;
 
-  const listRuntime = await query(sqlFirst);
+    const listRuntime = await query(sqlFirst);
 
-  /**
-   * Second query base on result from first query.
-   */
-  let queryStr = "";
-  listRuntime.forEach((v) => (queryStr += `"${v.runtime.toISOString()}", `));
-  queryStr = queryStr.slice(0, -2);
+    /**
+     * Second query base on result from first query.
+     */
+    let queryStr = "";
+    listRuntime.forEach((v) => (queryStr += `"${v.runtime.toISOString()}", `));
+    queryStr = queryStr.slice(0, -2);
 
-  const sqlSecond = `
+    const sqlSecond = `
     SELECT
       runtime,
       forecast_time,
@@ -160,32 +164,16 @@ app.post("/forecast_time", async (req, res, next) => {
     ORDER BY
       forecast_time`;
 
-  const result = await query(sqlSecond);
+    const result = await query(sqlSecond);
 
-  // await connection.end();
+    // await connection.end();
 
-  try {
     res.json(result);
   } catch (err) {
     console.error(`Error while query forecast_time`, err.message);
+    // Passes errors into the error handler
     next(err);
   }
-});
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
 });
 
 app.listen(port, () => {
